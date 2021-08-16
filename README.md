e572bdc22c56e12b5217a85bba3e77d48037216f
Ali Alhoshaiyan <alialhoshaiyan@gmail.com>
2021-08-16 20:37:47 +0300

    Removed complicated HTTP libs and used the more verbose HttpURLConnection

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Moyasar Android SDK
 
+Moyasar's official Android SDK
+
+## Installation
+
+// Using Jetpack
+
+## Requirements
+
+// Internet Permissions
+// ViewModel support
+// Android Jetpack
+
+## Credit Card Basic Usage
+
+## Stc Pay Basic Usage
--- sdk/src/main/java/com/moyasar/android/sdk/payment/PaymentService.kt
+++ sdk/src/main/java/com/moyasar/android/sdk/payment/PaymentService.kt
@@ -1,18 +1,42 @@
 package com.moyasar.android.sdk.payment
 
+import com.google.gson.Gson
+import com.moyasar.android.sdk.exceptions.ApiException
+import com.moyasar.android.sdk.extensions.postJson
+import com.moyasar.android.sdk.extensions.setBasicAuth
+import com.moyasar.android.sdk.payment.models.ErrorResponse
 import com.moyasar.android.sdk.payment.models.Payment
 import com.moyasar.android.sdk.payment.models.PaymentRequest
-import khttp.post
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
+import java.net.HttpURLConnection
+import java.net.URL
 
-class PaymentService(private val baseUrl: String) {
-    suspend fun create(request: PaymentRequest): Payment {
+class PaymentService(
+    private val apiKey: String,
+    private val baseUrl: String
+) {
+    private val gson = Gson()
+
+    suspend fun create(request: PaymentRequest): Payment = withContext(Dispatchers.IO) {
         val createUrl = getResourceUrl("v1/payments")
-        val response = post(createUrl, json = request)
+        val client = URL(createUrl).openConnection() as HttpURLConnection
 
+        client.setBasicAuth(apiKey, "")
+        val response = client.postJson(request)
+
+        if (response.statusCode !in 200..299) {
+            throw ApiException(
+                gson.fromJson(response.text, ErrorResponse::class.java)
+            )
+        }
+
+        gson.fromJson(response.text, Payment::class.java)
     }
 
     private fun getResourceUrl(url: String): String {
-        return baseUrl.trimEnd('/').trimEnd() +
+        return baseUrl.trimEnd('/').trimEnd() + "/" +
             url.trimStart('/').trimStart()
     }
 }
