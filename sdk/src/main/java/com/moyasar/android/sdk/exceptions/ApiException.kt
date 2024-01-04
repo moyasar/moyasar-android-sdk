abd31314768d32c1f2fbff6bcd772228184f7858
Abdulaziz Alrabiah <afalrabiah98@gmail.com>
2024-01-04 21:58:53 +0300

    Setup project

--- sdk/src/main/java/com/moyasar/android/sdk/exceptions/ApiException.kt
+++
@@ -1,5 +0,0 @@
-package com.moyasar.android.sdk.exceptions
-
-import com.moyasar.android.sdk.payment.models.ErrorResponse
-
-class ApiException(val response: ErrorResponse) : MoyasarException()
---
+++ sdkdriver/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.moyasarandroidsdk"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.moyasarandroidsdk"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = false
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    //noinspection GradleCompatible
+    implementation("com.android.support:appcompat-v7:28.0.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("com.android.support.test:runner:1.0.2")
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+}
