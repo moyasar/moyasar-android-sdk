7f3f7c404a2580eb90fc0d8441e160125e503b00
Mahmoud Ashraf <ashraaf.mahmoud@gmail.com>
2024-09-22 17:14:55 +0300

    Finish STC Pay Button UI Logic

--- sdkdriver/src/main/java/com/moyasar/android/sdkdriver/CheckoutViewModel.kt
+++ sdkdriver/src/main/java/com/moyasar/android/sdkdriver/CheckoutViewModel.kt
@@ -3,16 +3,17 @@ package com.moyasar.android.sdkdriver
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.os.Parcelable
-import com.moyasar.android.sdk.presentation.model.PaymentConfig
-import com.moyasar.android.sdk.domain.entities.PaymentResult
+import com.moyasar.android.sdk.creditcard.presentation.model.PaymentConfig
+import com.moyasar.android.sdk.core.domain.entities.PaymentResult
 import com.moyasar.android.sdk.core.extensions.default
-import com.moyasar.android.sdk.data.models.Payment
-import com.moyasar.android.sdk.presentation.view.fragments.PaymentFragment
+import com.moyasar.android.sdk.core.data.response.PaymentResponse
+import com.moyasar.android.sdk.stcpay.presentation.view.fragments.EnterMobileNumberFragment
+import com.moyasar.android.sdk.creditcard.presentation.view.fragments.PaymentFragment
 import kotlinx.parcelize.Parcelize
 
 class CheckoutViewModel : ViewModel() {
     val status = MutableLiveData<Status>().default(Status.Idle)
-    private val payment = MutableLiveData<Payment?>()
+    private val payment = MutableLiveData<PaymentResponse?>()
     private val config = PaymentConfig(
         amount = 100000,
         currency = "SAR",
@@ -29,21 +30,36 @@ class CheckoutViewModel : ViewModel() {
     // For demo purposes only
     private lateinit var activity: CheckoutActivity
     private lateinit var paymentFragment: PaymentFragment
+    private lateinit var enterMobileNumberFragment: EnterMobileNumberFragment
 
-    private fun onPaymentSheetResult(result: PaymentResult) {
+    private fun onCreditCardPaymentResult(result: PaymentResult) {
         activity.runOnUiThread {
             activity.supportFragmentManager.beginTransaction().remove(paymentFragment).commit()
         }
 
+        handlePaymentResult(result)
+    }
+
+    private fun onSTCPayPaymentResult(result: PaymentResult) {
+        activity.runOnUiThread {
+            activity.supportFragmentManager.beginTransaction().remove(enterMobileNumberFragment).commit()
+        }
+
+        handlePaymentResult(result)
+    }
+
+    private fun handlePaymentResult(result: PaymentResult) {
         when (result) {
             PaymentResult.Canceled -> {
                 status.value = Status.Failed(Exception("User canceled"))
                 println("User canceled")
             }
+
             is PaymentResult.Failed -> {
                 status.value = Status.Failed(Exception("Payment failed"))
                 println("Payment failed: ${result.error}")
             }
+
             is PaymentResult.Completed -> {
                 payment.value = result.payment
                 println("Payment: ${result.payment}")
@@ -51,24 +67,37 @@ class CheckoutViewModel : ViewModel() {
                 when (result.payment.status) {
                     "paid", "authorized" -> status.value = Status.Success
                     else -> {
-                        status.value = Status.Failed(Exception("Payment status isn't paid or authorized"))
+                        status.value =
+                            Status.Failed(Exception("Payment status isn't paid or authorized"))
                         println("Payment status isn't 'paid' or 'authorized', it's: '${result.payment.status}'")
                     }
                 }
             }
+
             is PaymentResult.CompletedToken -> {
                 println("Got newly created token: '${result.token.id}'")
             }
         }
     }
 
-    fun beginDonation(activity: CheckoutActivity, id: Int) {
+    fun beginDonationWithCreditCard(activity: CheckoutActivity, id: Int) {
         this.activity = activity
 
-        this.paymentFragment = PaymentFragment.newInstance(activity.application, config) { this.onPaymentSheetResult(it) }
+        this.paymentFragment = PaymentFragment.newInstance(activity.application, config) { this.onCreditCardPaymentResult(it) }
 
         activity.supportFragmentManager.beginTransaction().apply {
             replace(id, paymentFragment)
+            commit()
+        }
+    }
+
+    fun beginDonationWithSTC(activity: CheckoutActivity, id: Int) {
+        this.activity = activity
+
+        this.enterMobileNumberFragment = EnterMobileNumberFragment.newInstance(activity.application, config) { this.onSTCPayPaymentResult(it) }
+
+        activity.supportFragmentManager.beginTransaction().apply {
+            replace(id, enterMobileNumberFragment)
             commit()
         }
     }
