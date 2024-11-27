d0f348866598fec18509a06db5ac91babe4a683a
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2024-11-27 14:27:56 +0200

    increase publishing version to 1.0.5

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -12,7 +12,7 @@ publishing {
         create<MavenPublication>("MavenPublication") {
             groupId = "com.moyasar"
             artifactId = "android-sdk"
-            version = "1.0.4"
+            version = "1.0.5"
             afterEvaluate {
                 from(components["release"])
             }
