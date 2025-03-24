4617cbfb496156698ca9cff32872ccc4d197b541
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2025-03-24 08:36:28 +0200

    Add serialized name for PaymentSource classes

--- sdk/src/main/java/com/moyasar/android/sdk/stcpay/data/models/sources/STCPayPaymentSource.kt
+++ sdk/src/main/java/com/moyasar/android/sdk/stcpay/data/models/sources/STCPayPaymentSource.kt
@@ -1,8 +1,16 @@
 package com.moyasar.android.sdk.stcpay.data.models.sources
 
 import com.moyasar.android.sdk.core.data.PaymentSource
+import com.google.gson.annotations.SerializedName
 
 /**
  * Created by Mahmoud Ashraf on 22,September,2024
  */
-data class STCPayPaymentSource(val mobile: String, val type: String = "stcpay") : PaymentSource
+
+data class STCPayPaymentSource(
+    @SerializedName("mobile")
+    val mobile: String,
+
+    @SerializedName("type")
+    val type: String = "stcpay"
+) : PaymentSource
