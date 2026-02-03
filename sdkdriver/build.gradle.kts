27b664638241ce70c26bd3f6f004b21d2cc14e48
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2026-02-03 02:50:03 +0200

    Enable proguard and shrinking in release variant

--- sdkdriver/build.gradle.kts
+++ sdkdriver/build.gradle.kts
@@ -20,8 +20,10 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
